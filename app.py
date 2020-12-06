eaed9955e25cb4ee0b9b39cbed66c4217fe9457e
PabloBispo <pablofernando@live.com>
2020-12-06 12:43:30 -0300

    testing deploy

--- app.py
+++ app.py
@@ -44,7 +44,3 @@ def post_something():
 @app.route('/')
 def index():
     return "<h1>Welcome to our server !!</h1>"
-
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
